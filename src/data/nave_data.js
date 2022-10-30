048bb9c628f6a718dea334eea8c637295b10e027
niilliiz <niloo.vatan.nv@gmail.com>
2022-10-30 13:39:08 +0330

    feat: isActive property

--- src/data/nave_data.js
+++ src/data/nave_data.js
@@ -1,28 +1,30 @@
 // const base = "";
 const NavLinks = [
-  { title: "Live", path: "/" },
-  { title: "Push", path: "/push" },
-  { title: "Notes", path: "/note" },
-  { title: "Link", path: "/link" },
+  { title: "Live", path: "/", isActive: true },
+  { title: "Push", path: "/push", isActive: false },
+  { title: "Notes", path: "/note", isActive: false },
+  { title: "Link", path: "/link", isActive: false },
   {
     title: "Shop",
     path: "/shop",
+    isActive: true,
     links: [
-      { title: "Overview", path: "/shop/" },
-      { title: "Live", path: "/shop/live" },
-      { title: "Push", path: "/shop/push" },
+      { title: "Overview", path: "/shop/", isActive: true },
+      { title: "Live", path: "/shop/live", isActive: false },
+      { title: "Push", path: "/shop/push", isActive: false },
       {
         title: "Educational offers",
         path: "/shop/educational-offers",
+        isActive: false,
       },
-      { title: "Max for live", path: "/shop/Max-for-live" },
-      { title: "Making music", path: "/shop/making-music" },
-      { title: "Merchandise", path: "/shop/merchandise" },
+      { title: "Max for live", path: "/shop/Max-for-live", isActive: false },
+      { title: "Making music", path: "/shop/making-music", isActive: false },
+      { title: "Merchandise", path: "/shop/merchandise", isActive: false },
     ],
   },
-  { title: "Packs", path: "/packs" },
-  { title: "Help", path: "/help" },
-  { title: "More+", path: "/more" },
+  { title: "Packs", path: "/packs", isActive: true },
+  { title: "Help", path: "/help", isActive: false },
+  { title: "More+", path: "/more", isActive: false },
 ];
 
 export default NavLinks;
