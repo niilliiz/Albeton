67a3223ff83681ebfe870ae478fc9bd495d5cfbb
niilliiz <niloo.vatan.nv@gmail.com>
2022-11-07 12:30:46 +0330

    feat: cart page created

--- src/components/cart_icon/cart_icon.js
+++ src/components/cart_icon/cart_icon.js
@@ -2,9 +2,10 @@ import React from "react";
 import { ShoppingCartSimple } from "phosphor-react";
 
 import styles from "./cart_icon_style.module.scss";
+import { Link } from "react-router-dom";
 const CartIcon = ({ onClick = null }) => {
   return (
-    <div className={styles.cart}>
+    <Link to="/cart" className={styles.cart}>
       <div className={styles.cart__responsive}>
         <ShoppingCartSimple
           className={styles["icon--small"]}
@@ -13,7 +14,7 @@ const CartIcon = ({ onClick = null }) => {
         />
         <ShoppingCartSimple size={24} className={styles["icon--large"]} />
       </div>
-    </div>
+    </Link>
   );
 };
 export default CartIcon;
