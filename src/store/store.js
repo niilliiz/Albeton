ca472a8696ab7a531c93df8da55cfb15ed70dc74
niilliiz <niloo.vatan.nv@gmail.com>
2022-11-14 11:12:00 +0330

    feat: redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -2,15 +2,19 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducers";
-import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 
 const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
-const composedEnhancers = composeWithDevTools(
-  compose(applyMiddleware(...middleWares))
-);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 const persistConfig = {
   key: "root",
