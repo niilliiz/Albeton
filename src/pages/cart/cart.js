d79c53c6bb9da696855bc59bffee9754de0ca4e4
niilliiz <niloo.vatan.nv@gmail.com>
2022-11-17 13:21:30 +0330

    feat: empty state

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -20,20 +20,36 @@ const Cart = () => {
     <main className={styles.cart}>
       <div className={styles.cart__main}>
         <h1 className="h1">Cart</h1>
-        <div className={styles.cards}>
-          {cartItems.map((item) => (
-            <CartItem product={item} key={item.id} />
-          ))}
-        </div>
-        <div className={`${styles.footer}`}>
-          <div className={`${styles.footer__detail}`}>
-            <div className={`${styles.row} h2 fw--700`}>
-              <span>Total</span>
-              <span>EUR {cartTotal}</span>
+        {cartItems?.length > 0 ? (
+          <>
+            <div className={styles.cards}>
+              {cartItems.map((item) => (
+                <CartItem product={item} key={item.id} />
+              ))}
             </div>
+            <div className={`${styles.footer}`}>
+              <div className={`${styles.footer__detail}`}>
+                <div className={`${styles.row} h2 fw--700`}>
+                  <span>Total</span>
+                  <span>EUR {cartTotal}</span>
+                </div>
+              </div>
+              <button className={styles.button}>Proceed to Checkout</button>
+            </div>
+          </>
+        ) : (
+          <div className="fw--700 h3 display--inline">
+            <span style={{ marginInlineEnd: "0.5rem" }}>
+              Your cart is empty.
+            </span>
+            <Link
+              className="clr--primary"
+              content="Start Shopping"
+              link="/packs"
+              weight="bold"
+            />
           </div>
-          <button className={styles.button}>Proceed to Checkout</button>
-        </div>
+        )}
       </div>
       <div className={styles.cart__footer}>
         <div className={styles.payment}>
