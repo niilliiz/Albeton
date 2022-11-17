f0715adbcc38463aa3b49e5b9f29330fa5ff2837
niilliiz <niloo.vatan.nv@gmail.com>
2022-11-17 18:38:57 +0330

    fix: scroll bug

--- src/components/UI/disable_scroll.js
+++ src/components/UI/disable_scroll.js
@@ -1,8 +1,10 @@
 const DisableScroll = ({ isHidden = false }) => {
   if (isHidden) {
     document.body.style.overflow = "hidden";
+    document.body.style.position = "fixed";
   } else {
     document.body.style.overflow = "auto";
+    document.body.style.position = "static";
   }
 };
 export default DisableScroll;
