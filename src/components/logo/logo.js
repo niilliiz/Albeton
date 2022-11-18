c2c2010ce49fa53ee7305ff1da9125ce5b00bcec
niilliiz <niloo.vatan.nv@gmail.com>
2022-11-18 18:04:19 +0330

    fix: removed unsude line

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -16,7 +16,6 @@ const Logo = ({
         height={height}
         viewBox="0 0 45 21"
       >
-        <title id="logo">Ableton Homepage</title>
         <path d="M0 0h3v21H0zM6 0h3v21H6zM12 0h3v21h-3zM18 0h3v21h-3zM24 18h21v3H24zM24 12h21v3H24zM24 6h21v3H24zM24 0h21v3H24z"></path>
       </svg>
     </Link>
