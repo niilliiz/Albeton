2dc549843cfc353d1ac5a501641504d40115bbd7
niilliiz <niloo.vatan.nv@gmail.com>
2022-11-18 18:02:05 +0330

    feat: React.memo

--- src/components/link/link.js
+++ src/components/link/link.js
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { memo } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 import { CaretRight } from "phosphor-react";
 
 import styles from "./link_style.module.scss";
 
-const Link = ({
-  className = "",
-  link = "#",
-  content,
-  size = "12",
-  weight = "regular",
-  underline = false,
-}) => {
-  return (
-    <RouterLink className={`${styles.link} ${className}`} to={link}>
-      <p style={{ textDecoration: underline ? "underline" : "none" }}>
-        {content}
-      </p>
-      <CaretRight size={size} weight={weight} />
-    </RouterLink>
-  );
-};
+const Link = memo(
+  ({
+    className = "",
+    link = "#",
+    content,
+    size = "12",
+    weight = "regular",
+    underline = false,
+  }) => {
+    return (
+      <RouterLink className={`${styles.link} ${className}`} to={link}>
+        <p style={{ textDecoration: underline ? "underline" : "none" }}>
+          {content}
+        </p>
+        <CaretRight size={size} weight={weight} />
+      </RouterLink>
+    );
+  }
+);
 export default Link;
