fc9076768ad828fdc7b250a1adde386de5567f39
niilliiz <niloo.vatan.nv@gmail.com>
2022-11-19 19:42:21 +0330

    refactor: import font family

--- src/components/UI/disable_scroll.js
+++ src/components/UI/disable_scroll.js
@@ -2,9 +2,13 @@ const DisableScroll = ({ isHidden = false }) => {
   if (isHidden) {
     document.body.style.overflow = "hidden";
     document.body.style.position = "fixed";
+    document.body.style.height = "100vh";
+    document.body.style.width = "100vw";
   } else {
     document.body.style.overflow = "auto";
     document.body.style.position = "static";
+    document.body.style.height = "auto";
+    document.body.style.width = "auto";
   }
 };
 export default DisableScroll;
