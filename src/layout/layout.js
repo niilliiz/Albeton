861d46fe290213a77e98542ff56d640fa97a19aa
niilliiz <niloo.vatan.nv@gmail.com>
2022-11-19 16:23:24 +0330

    refactor: style module

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -2,11 +2,11 @@ import { Outlet } from "react-router-dom";
 import Toast from "../components/toast/toast";
 import Footer from "../sections/footer/footer";
 import Header from "../sections/header/header";
-import "./layout.scss";
+import styles from "./layout_style.module.scss";
 
 function Layout() {
   return (
-    <div className="layout">
+    <div className={styles.layout}>
       <Toast />
       <Header />
       <Outlet />
