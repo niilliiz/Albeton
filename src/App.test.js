2ca9b0ba3f4e810764ce57daa490d570d8b0385b
niilliiz <niloo.vatan.nv@gmail.com>
2022-12-04 19:01:12 +0330

    feat: sing-up and sing-in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,65 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SignUp from "./pages/authentication/sign-up/sign-up";
+import SignIn from "./pages/authentication/sing-in/sign-in";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// --------------------SIGNUP TESTS---------------
+
+describe("SIGN UP TEST", () => {
+  it("Input must be empty", () => {
+    render(<SignUp />);
+    const textBoxRoles = screen.getAllByRole("textbox");
+    const passwordRelatedBox = screen.getAllByLabelText(/password/i);
+    [...textBoxRoles, ...passwordRelatedBox].forEach((element) =>
+      expect(element.value).toBe("")
+    );
+  });
+
+  it("Weak password", async () => {
+    render(<SignUp />);
+    const passwordElement = screen.getByLabelText("Password *");
+
+    expect(
+      screen.queryByText(
+        "Weak password. It must contain at least 6 characters."
+      )
+    ).toBeNull();
+
+    await userEvent.type(passwordElement, "12345");
+
+    const helperTextElement = screen.queryByText(
+      "Weak password. It must contain at least 6 characters."
+    );
+    expect(helperTextElement).toBeInTheDocument();
+  });
+
+  it("Not equal password", async () => {
+    render(<SignUp />);
+
+    const passwordElement = screen.getByLabelText("Password *");
+    const confirmPasswordElement = screen.getByLabelText("Confirm password *");
+
+    expect(screen.queryByText("Passwords don't match. Try again!")).toBeNull();
+
+    await userEvent.type(passwordElement, "123456");
+    await userEvent.type(confirmPasswordElement, "12345");
+
+    const helperTextElement = screen.queryByText(
+      "Passwords don't match. Try again!"
+    );
+    expect(helperTextElement).toBeInTheDocument();
+  });
+});
+
+// --------------------SIGN IN TESTS---------------
+
+describe("SIGN IN TEST", () => {
+  it("Input must be empty", () => {
+    render(<SignIn />);
+    const emailInput = screen.getByRole("textbox");
+    const passwordInput = screen.getByLabelText(/password/i);
+    [emailInput, passwordInput].forEach((element) =>
+      expect(element.value).toBe("")
+    );
+  });
 });
