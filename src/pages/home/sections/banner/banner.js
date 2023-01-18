e726d5c21e2903f7a8e9859f735fbcde8c9352b0
niilliiz <niloo.vatan.nv@@gmail.com>
2023-01-18 18:47:50 +0330

    fix: hasLink

--- src/pages/home/sections/banner/banner.js
+++ src/pages/home/sections/banner/banner.js
@@ -6,27 +6,27 @@ const BannerSection = (props) => {
   return (
     <section role="banner" className={styles.banner}>
       <Cell
-        cellClassName={styles.cell__left}
+        hasLink
         weight="bold"
         underline={true}
         content="Checkout the roundup"
-        haveLink
+        cellClassName={styles.cell__left}
         title="A Look at Eight Standout Features and Video from 2022"
       />
       <Cell
-        cellClassName={styles.cell__middle}
+        hasLink
         weight="bold"
         underline={true}
         content="Learn his creative process"
-        haveLink
+        cellClassName={styles.cell__middle}
         title='Aril Brikha: Remaking "Groove La Chord" with Live and Note'
       />
       <Cell
-        cellClassName={styles.cell__right}
+        hasLink
         weight="bold"
         underline={true}
         content="Watch the video"
-        haveLink
+        cellClassName={styles.cell__right}
         title="Made in Ableton Live: Ori Moto"
       />
     </section>
