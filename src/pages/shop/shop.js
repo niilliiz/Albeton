dedda4685d18c958271fb8efa420b81b51a5e1dd
niilliiz <niloo.vatan.nv@@gmail.com>
2023-01-18 18:48:02 +0330

    feat: cell component

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import Cell from "../../components/cell/cell";
 import Link from "../../components/link/link";
 
 import PaymentLogos from "../../components/payment_logo/payment_logo";
@@ -9,47 +10,41 @@ import styles from "./shop_style.module.scss";
 const Shop = () => {
   return (
     <main className={styles.shop}>
-      <div className={styles.card} data-card="live">
-        <p className="h1">Live</p>
-        <p className="h4">
-          Fast, fluid and flexible software for music creation and performance.
-        </p>
-        <Link
-          className="h4 fw--700"
-          weight="bold"
-          content="Buy now"
-          underline
-        />
-      </div>
-      <div className={styles.card} data-card="push">
-        <p className="h1">Push</p>
-        <p className="h4">
-          Powered by live, push puts all the fundamental elements of
-          music-making at your fingertips
-        </p>
-        <Link
-          className="h4 fw--700"
-          weight="bold"
-          content="Buy now"
-          underline
-        />
-      </div>
-      <div data-card="student" className={styles.card}>
-        <p className="h2">
-          Students and teachers - save up to 40% with out education offers
-        </p>
-        <Link className={`h4 clr--primary fw--700`} content="Learn more" />
-      </div>
-      <div className={styles.card} data-card="max-for-live">
-        <p className="h1">Max for Live</p>
-        <p className="h4">
-          Discover new instruments, effects and customizations for Live
-        </p>
-        <Link
-          className={`h4 clr--primary fw--700`}
-          weight="bold"
-          content="Learn more"
-        />
+      <Cell
+        hasLink
+        title="Live"
+        weight="bold"
+        underline={true}
+        content="Buy now"
+        cellClassName={`${styles.card} ${styles.live}`}
+        subTitle="Fast, fluid and flexible software for music creation and performance."
+      />
+      <Cell
+        hasLink
+        title="Push"
+        weight="bold"
+        underline={true}
+        content="Buy now"
+        cellClassName={`${styles.card} ${styles.push}`}
+        subTitle="Powered by live, push puts all the fundamental elements of music-making at your fingertips."
+      />
+      <Cell
+        hasLink
+        weight="bold"
+        content="Learn more"
+        linkClassName="h4 clr--primary fw--700"
+        cellClassName={`${styles.card} ${styles.student}`}
+        title="Students and teachers - save up to 40% with out education offers"
+      />
+      <Cell
+        hasLink
+        weight="bold"
+        content="Learn more"
+        title="Max for Live"
+        cellClassName={`${styles.card} ${styles["max-for-live"]}`}
+        subTitle="Discover new instruments, effects and customizations for Live"
+        linkClassName="h4 clr--primary fw--700"
+      >
         <p className="h4 fw--700">EUR 149</p>
         <button className={styles.card__button}>Buy now</button>
         <p className="h4 fw--700">EUR 1479</p>
@@ -57,13 +52,15 @@ const Shop = () => {
         <a href="#" className="h5 text--underline clr--neutral">
           What is a crossgrade?
         </a>
-      </div>
-      <div className={styles.card} data-card="making-music">
-        <p className="h1">Making Music</p>
-        <p className="h4">
-          74 Creative Strategies for Electronic Music Producers. A book by
-          Dennis DeSanits.
-        </p>
+      </Cell>
+
+      <Cell
+        weight="bold"
+        title="Making Music"
+        cellClassName={`${styles.card} ${styles["making-music"]}`}
+        subTitle="74 Creative Strategies for Electronic Music Producers. A book by
+          Dennis DeSanits."
+      >
         <p className="h4">
           Download for free in the following formats:
           <span className="clr--primary">
@@ -91,47 +88,54 @@ const Shop = () => {
         <span className="h5 clr--neutral">
           Published b Albeton, 340 pages, hardcover
         </span>
-      </div>
-      <div className={styles.card} data-card="packs">
-        <p className="h1">Packs</p>
-        <p className="h4">
-          Expand your studio with sounds, tools and instruments for Live
-        </p>
-        <Link className="h4 clr--primary" content="Browse Packs" />
-      </div>
-      <div className={styles.card} data-card="merchandise">
-        <p className="h1">Merchandise</p>
-        <p className="h4">Albeton apparel, hardware accessories more.</p>
-        <Link className="h4 clr--primary" content="Browse Merchandise" />
-      </div>
-      <div className={styles.card} data-card="payment-methods">
-        <p className="h1">Payments methods</p>
-        <span className="h4">
-          We accept payments via Credit Card, PayPal, iDEAL, Google Pay.
-          <Link
-            className="clr--primary h4 display--inline"
-            content="Learn more"
-          />
-        </span>
+      </Cell>
+      <Cell
+        hasLink
+        weight="bold"
+        title="Packs"
+        content="Browse Packs"
+        linkClassName="h4 clr--primary"
+        cellClassName={`${styles.card} ${styles.packs}`}
+        subTitle="Expand your studio with sounds, tools and instruments for Live"
+      />
+      <Cell
+        hasLink
+        weight="bold"
+        title="Merchandise"
+        content="Browse Merchandise"
+        linkClassName="h4 clr--primary"
+        cellClassName={`${styles.card} ${styles.merchandise}`}
+        subTitle="Albeton apparel, hardware accessories more."
+      />
+      <Cell
+        hasLink
+        weight="bold"
+        content="Learn more"
+        title="Payment Method"
+        linkClassName="clr--primary h4 display--inline"
+        cellClassName={`${styles.card} ${styles["payment-methods"]}`}
+        subTitle="We accept payments via Credit Card, PayPal, iDEAL, Google Pay."
+      >
         <div className={styles["payment--wrapper"]}>
           <PaymentLogos />
         </div>
-      </div>
-      <div className={styles.card} data-card="security">
-        <p className="h1">Secure shopping</p>
-        <p className="h4">
-          Shopping at the Albeton webshop is safe: Your customer data is
-          transferred using a secure connections
-        </p>
-      </div>
-      <div data-card="question" className={styles.card}>
-        <p className="h1">Questions?</p>
-        <p className="h4">
-          Don't hesitate to get in touch with us if you have any questions
-          before you buy Live or Push
-        </p>
-        <Link className={`clr--primary h4`} content="Ask us a question" />
-      </div>
+      </Cell>
+      <Cell
+        title="Secure Shopping"
+        cellClassName={`${styles.card} ${styles.security}`}
+        subTitle="Shopping at the Albeton webshop is safe: Your customer data is
+          transferred using a secure connections."
+      />
+      <Cell
+        hasLink
+        weight="bold"
+        title="Questions"
+        content="Ask us a question"
+        linkClassName="clr--primary h4"
+        cellClassName={`${styles.card} ${styles.question}`}
+        subTitle="Don't hesitate to get in touch with us if you have any questions
+          before you buy Live or Push"
+      />
     </main>
   );
 };
