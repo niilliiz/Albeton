5cd2bfc0dddbd16f2c8f1b1bc4d33c72c7ca6b5f
niilliiz <niloo.vatan.nv@@gmail.com>
2023-01-18 18:47:50 +0330

    fix: hasLink

--- src/components/cell/cell.js
+++ src/components/cell/cell.js
@@ -4,20 +4,22 @@ import Link from "../link/link";
 
 import styles from "./cell_style.module.scss";
 const Cell = ({
-  cellName = "",
-  cellClassName = "",
   title = "",
+  cellName = "",
   subTitle = "",
-  haveLink = false,
+  hasLink = false,
+  children = null,
   linkClassName = "",
+  cellClassName = "",
   ...otherProps
 }) => {
   return (
     <div className={`${styles.cell} ${cellClassName}`} data-cell={cellName}>
-      {title && <h2>{title}</h2>}
-      {subTitle && <h3>{subTitle}</h3>}
+      {title && <h2 className="h1">{title}</h2>}
+      {subTitle && <h3 className="h4">{subTitle}</h3>}
 
-      {haveLink && <Link className={linkClassName} {...otherProps} />}
+      {hasLink && <Link className={linkClassName} {...otherProps} />}
+      {children}
     </div>
   );
 };
