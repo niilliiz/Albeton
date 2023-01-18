f215c21c8dd766bed5f12bdf69d203a5a0b0cff1
niilliiz <niloo.vatan.nv@@gmail.com>
2023-01-18 17:58:06 +0330

    feat: haveLink

--- src/pages/home/sections/banner/banner.js
+++ src/pages/home/sections/banner/banner.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Cell from "../../../../components/cell/cell";
 import styles from "./banner_style.module.scss";
-/**
- * left img: https://ableton-production.imgix.net/uploads/homepage-teasers/392_Year_RoundUp3_xdKVXy1.jpg?auto=format&fit=crop&fm=jpg&h=535&ixjsv=1.1.3&w=803
- * middle img: https://ableton-production.imgix.net/uploads/homepage-teasers/392_Year_RoundUp5_KtMYBl8.jpg?auto=format&fit=crop&fm=jpg&h=306&ixjsv=1.1.3&w=459
- * right img: https://ableton-production.imgix.net/uploads/homepage-teasers/392_Year_RoundUp4_I3ynQcZ.jpg?auto=format&fit=crop&fm=jpg&h=306&ixjsv=1.1.3&w=459
- */
 
 const BannerSection = (props) => {
   return (
@@ -15,6 +10,7 @@ const BannerSection = (props) => {
         weight="bold"
         underline={true}
         content="Checkout the roundup"
+        haveLink
         title="A Look at Eight Standout Features and Video from 2022"
       />
       <Cell
@@ -22,6 +18,7 @@ const BannerSection = (props) => {
         weight="bold"
         underline={true}
         content="Learn his creative process"
+        haveLink
         title='Aril Brikha: Remaking "Groove La Chord" with Live and Note'
       />
       <Cell
@@ -29,6 +26,7 @@ const BannerSection = (props) => {
         weight="bold"
         underline={true}
         content="Watch the video"
+        haveLink
         title="Made in Ableton Live: Ori Moto"
       />
     </section>
